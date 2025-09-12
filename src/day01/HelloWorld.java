d2ce8d184918c1a5e9f7f5fc739c8a3570cdd16a
kerrami45 <146778938+kerrami45@users.noreply.github.com>
2025-09-11 22:39:24 -0400

    Update HelloWorld.java
    Hello World Update

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -3,5 +3,8 @@ package day01;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello school");
+        System.out.println("Hello friends");
+        
     }
 }
