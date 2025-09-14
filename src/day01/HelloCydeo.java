d1328216d87074926203d9e2f837f48169c99ba3
kerrami45 <146778938+kerrami45@users.noreply.github.com>
2025-09-13 20:17:17 -0400

    Update HelloCydeo.java
    updater

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -4,6 +4,9 @@ public class HelloCydeo {
     public static void main(String[] args) {
         System.out.println("Hello Cydeo");
         System.out.println("Hello Friends");
+        System.out.println("Hello Friends");
+        
+
          
     }
 }
